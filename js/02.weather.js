2c4cce515207642ef830f343fa58a2053f73aff0
booldook <booldook@gmail.com>
2019-12-04 17:10:24 +0900

    02.weather

--- js/02.weather.js
+++ js/02.weather.js
@@ -18,7 +18,40 @@ function getCity(res) {
 	}
 }
 
+// 화면전환
+function uiChg(view) {
+	switch(view) {
+		case "Daily":
+			$(".main-wrap").hide();
+			$(".daily-wrap").show();
+			$(".weekly-wrap").hide();
+			$(".nav-item").removeClass("active");
+			$(".nav-item").eq(1).addClass("active");
+			break;
+		case "Weekly":
+			$(".main-wrap").hide();
+			$(".daily-wrap").hide();
+			$(".weekly-wrap").show();
+			$(".nav-item").removeClass("active");
+			$(".nav-item").eq(2).addClass("active");
+			break;
+		default :
+			$(".main-wrap").show();
+			$(".daily-wrap").hide();
+			$(".weekly-wrap").hide();
+			$(".nav-item").removeClass("active");
+			$(".nav-item").eq(0).addClass("active");
+			break;
+	}
+}
+
+
+
 // 이벤트
+$(".nav-item").click(function(){
+	uiChg($(this).data("view"));
+});
+
 $("#cityId").change(function(){
 	$.ajax({
 		url: "https://api.openweathermap.org/data/2.5/weather",
