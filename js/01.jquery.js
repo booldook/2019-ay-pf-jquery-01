eb179c077199a7f26adea640b510351a3547d9e0
booldook <booldook@gmail.com>
2019-12-04 12:08:17 +0900

    first commit

---
+++ js/01.jquery.js
@@ -0,0 +1,15 @@
+/* 
+0. BOM(Browser Object Model)
+
+jQuery
+1. Selector - find(), children(), parent(), next(), prev(), eq(), index()
+2. Event - click(), mouseover(), mouseenter(), mouseleave(), scroll(), resize()
+3. Dimension - width(), height(), innerWidth(), innerHeight(), outerWidth(), outerHeight()
+4. Property - attr(), prop(), css(), addClass(), removeClass(), toggleClass()
+5. Animation - show(), hide(), toggle(), fadeIn(), fadeOut(), fadeToggle(), slideDown(), slideUp(), slideToggle(), animate()
+6. DOM(Document Object Model) - append(), apprendTo(), prepend(), prependTo(), html(), empty(), remove()
+
+js
+1. ES5
+2. ES6
+*/
